ccc32d944246c834fb4c95c9f7cc2c14948884d4
Rohit Gupta <rohitguptarg24@gmail.com>
2020-06-19 14:47:44 -0400

    Javadoc for Test Cases

--- src/test/MainControllerTest.java
+++ src/test/MainControllerTest.java
@@ -5,16 +5,25 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import junit.framework.TestCase;
 import main.MainController;
 
-public class MainControllerTest  {
-
+/**
+ * Test class for the Main Controller.
+ * 
+ * @author Team D
+ *
+ */
+public class MainControllerTest {
+	/** Initialize Main Controller */
 	MainController mainController = new MainController();
 
+	/**
+	 * Set up method called before running the test methods.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
-
 		MainController.data.add(4);
 		MainController.data.add(2);
 		MainController.data.add(3);
@@ -26,41 +35,75 @@ public class MainControllerTest  {
 		MainController.data.add(2);
 	}
 
+	/**
+	 * Test Case to evaluate the Min Value on the given set of data.
+	 */
 	@Test
 	public void findMinValueTest() {
 		assertEquals(2, mainController.findMinValue());
 	}
 
+	/**
+	 * Test Case to evaluate the Min Value on the given set of data.
+	 */
 	@Test
 	public void findMaxValueTest() {
 		assertEquals(5, mainController.findMaxValue());
 	}
 
+	/**
+	 * Test Case to evaluate the Mean on the given set of data. The delta of
+	 * 0.000009 is used to verify the preciseness of the result till 5 decimal
+	 * places.
+	 */
 	@Test
 	public void findMeanTest() {
-		assertEquals(2.875, mainController.findMean(),0.00001);
+		assertEquals(2.875, mainController.findMean(), 0.000009);
 	}
 
+	/**
+	 * Test Case to evaluate Mean Absolute Deviation on the given set of data. The
+	 * delta of 0.000009 is used to verify the preciseness of the result till 5
+	 * decimal places.
+	 */
 	@Test
 	public void findMeanAbsoluteDeviationTest() {
-		assertEquals(0.875, mainController.findMeanAbsoluteDeviation(),0.00001);
+		assertEquals(0.875, mainController.findMeanAbsoluteDeviation(), 0.000009);
 	}
 
+	/**
+	 * Test Case to evaluate the Median on the given set of data. The delta of
+	 * 0.000009 is used to verify the preciseness of the result till 5 decimal
+	 * places.
+	 */
 	@Test
 	public void findMedianTest() {
-		assertEquals(2.5, mainController.findMedian(),0.00001);
+		assertEquals(2.5, mainController.findMedian(), 0.000009);
 	}
 
+	/**
+	 * Test Case to evaluate the Standard Deviation on the given set of data. The
+	 * delta of 0.000009 is used to verify the preciseness of the result till 5
+	 * decimal places.
+	 */
 	@Test
 	public void findStandardDeviationTest() {
-		assertEquals(1.05326, mainController.findStandardDeviation(),0.00001);
+		assertEquals(1.05326, mainController.findStandardDeviation(), 0.000009);
 	}
 
+	/**
+	 * Test Case to evaluate the variance on the given set of data. The delta of
+	 * 0.000009 is used to verify the preciseness of the result till 5 decimal
+	 * places.
+	 */
 	@Test
 	public void findVarianceTest() {
-		assertEquals(1.10937, mainController.findVariance(),0.00001);
+		assertEquals(1.10937, mainController.findVariance(), 0.000009);
 	}
 
+	/**
+	 * Test Case to evaluate the Mode Value on the given set of data.
+	 */
 	@Test
 	public void findModeTest() {
 		assertEquals("2", mainController.findMode());
